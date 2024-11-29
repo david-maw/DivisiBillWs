e1b2ecb620dc78554db1f85e1351807ebb5beb2a
David Maw <david@maw.name>
2024-11-29 09:13:07 -0800

    Simplify VersionFunction
    No functional changes.

--- DivisiBillWs/VersionFunction.cs
+++ DivisiBillWs/VersionFunction.cs
@@ -1,49 +1,47 @@
-using DivisiBillWs.Generated;
+using DivisiBillWs.Generated; // The build time information  created by the msbuild of the project file
 using Microsoft.Extensions.Hosting;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace DivisiBillWs
+namespace DivisiBillWs;
+
+public class VersionFunction
 {
-    public class VersionFunction
-    {
-        private readonly ILogger _logger;
-        private readonly IHostEnvironment env;
-        private readonly bool IsDebug =
-        #if DEBUG
-                true;
+    private readonly ILogger _logger;
+    private readonly IHostEnvironment env;
+    private readonly bool IsDebug =
+    #if DEBUG
+            true;
 #else
-                false;
+            false;
 #endif
 
-        public VersionFunction(ILoggerFactory loggerFactory, IHostEnvironment envParam)
-        {
-            _logger = loggerFactory.CreateLogger<VersionFunction>();
-            env = envParam;
-        }
-        public static string BuildTime { get; } = DateTime.Parse(BuildEnvironment.BuildTimeString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.RoundtripKind).ToLocalTime().ToString();
+    public VersionFunction(ILoggerFactory loggerFactory, IHostEnvironment envParam)
+    {
+        _logger = loggerFactory.CreateLogger<VersionFunction>();
+        env = envParam;
+    }
+    public static string BuildTime { get; } = DateTime.Parse(BuildEnvironment.BuildTimeString, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.RoundtripKind).ToLocalTime().ToString();
 
-        /// Beware, according to https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference?tabs=blob#parallel-execution
-        /// a the function code may be simultaneously executed on multiple threads.
-        [Function("version")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
-        {
-            _logger.LogInformation("The 'version' web service function is processing a request.");
+    /// Beware, according to https://learn.microsoft.com/en-us/azure/azure-functions/functions-reference?tabs=blob#parallel-execution
+    /// a the function code may be simultaneously executed on multiple threads.
+    [Function("version")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    {
+        _logger.LogInformation("The 'version' web service function is processing a request.");
 
-            return await req.MakeResponseAsync(HttpStatusCode.OK,$""" 
-                Application: {env.ApplicationName} 
-                Application_Version: {typeof(VersionFunction).Assembly.GetName().Version}
-                NET_Version: {typeof(int).Assembly.GetName().Version}
-                Build_Time: {BuildTime}
-                Environment: {env.EnvironmentName}
-                Platform:{Environment.OSVersion.Platform}
-                Content_Path: {env.ContentRootPath}
-                Debug: {IsDebug}
-                Site_Name: {Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME")}
-                Play_Store_Key: {(string.IsNullOrEmpty(Generated.BuildInfo.PlayCredentialB64) ? "Missing" : "Present")} 
-                Cognitive_Services_Entry_Point: {(string.IsNullOrEmpty(Generated.BuildInfo.DivisiBillCognitiveServicesEndpoint) ? "Missing" : "Present")} 
-                Cognitive_Services_Key: {(string.IsNullOrEmpty(Generated.BuildInfo.DivisiBillCognitiveServicesKey) ? "Missing" : "Present")} 
-                Sentry_DSN: {(string.IsNullOrEmpty(Generated.BuildInfo.DivisiBillSentryDsn) ? "Missing" : "Present")} 
-                """);
-        }
+        return await req.MakeResponseAsync(HttpStatusCode.OK,$""" 
+            Application: {env.ApplicationName} 
+            Application_Version: {typeof(VersionFunction).Assembly.GetName().Version}
+            NET_Version: {typeof(int).Assembly.GetName().Version}
+            Build_Time: {BuildTime}
+            Environment: {env.EnvironmentName}
+            Platform:{Environment.OSVersion.Platform}
+            Content_Path: {env.ContentRootPath}
+            Debug: {IsDebug}
+            Site_Name: {Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME")}
+            Play_Store_Key: {(string.IsNullOrEmpty(Generated.BuildInfo.PlayCredentialB64) ? "Missing" : "Present")} 
+            Cognitive_Services_Entry_Point: {(string.IsNullOrEmpty(Generated.BuildInfo.DivisiBillCognitiveServicesEndpoint) ? "Missing" : "Present")} 
+            Cognitive_Services_Key: {(string.IsNullOrEmpty(Generated.BuildInfo.DivisiBillCognitiveServicesKey) ? "Missing" : "Present")} 
+            Sentry_DSN: {(string.IsNullOrEmpty(Generated.BuildInfo.DivisiBillSentryDsn) ? "Missing" : "Present")} 
+            """);
     }
 }
